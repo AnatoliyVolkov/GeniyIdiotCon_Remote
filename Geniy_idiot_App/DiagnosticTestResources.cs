4a1d04e6fb8559d13bd5599513d959da933e7353
Анатолий Волков <anatoliy.irl@yandex.ru>
2025-09-09 21:16:41 +0300

    Add project files.

---
+++ Geniy_idiot_App/DiagnosticTestResources.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeniyIdiotApp
+{
+    public static class DiagnosticTestResources
+    {
+        public static List<string> Questions = new List<string>()
+    {
+        "Сколько будет два плюс два умноженное на два?",
+        "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?",
+        "На двух руках 10 пальцев. Сколько пальцев на 5 руках?",
+        "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?",
+        "Пять свечей горело, три потухли. Сколько свечей осталось?"
+    };
+        public static List<int> Answers = new List<int>() { 6, 9, 25, 60, 2 };
+        public static List<string> Diagnoses = new List<string>()
+    {
+        "Кретин",
+        "Идиот",
+        "Дурак",
+        "Нормальный",
+        "Талант",
+        "Гений",
+    };
+
+        public static List<int> GetMixiForQuestion()
+        {
+            var randomQuestion = new Random();
+            var questionIndexes = Enumerable.Range(0, Questions.Count).ToList();
+            for (var i = questionIndexes.Count - 1 ; i > 0 ; i--)
+            {
+                var j = randomQuestion.Next(i + 1);
+                (questionIndexes[i], questionIndexes[j]) = (questionIndexes[j], questionIndexes[i]);
+            }
+            return questionIndexes;
+        }
+        public static string UserInput(string name)
+        {
+
+            if (string.IsNullOrEmpty(name) || short.TryParse(name, out var number))
+            {
+                throw new Exception("Нельзя вводить числа и оставлять поле пустым, будьте внимательней");
+            }
+            else
+            {
+                var nameUser = name.Substring(0, 1).ToUpper() + name.Substring(1).ToLower();
+                return nameUser;
+            }
+        }
+        public static bool GetRestartQuestion(string answer, string name)
+        {
+            if (answer != "Да" && answer != "дА" && answer != "да" && answer != "ДА")
+            {
+                Console.WriteLine($"Спасибо {name}, что прошли наш тест. Всего хорошего.");
+                return false;
+            }
+            else return true;
+
+        }
+    }
+
+}
---
+++ Geniy_idiot_App/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace GeniyIdiotApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добрый день, вы сейчас будете проходить тест на определение вашей гениальности.\nПожалуйста введите свое имя.");
+            var userName = GetValidUserName();
+            Console.WriteLine($"Добро пожаловать {userName}! Мы приступаем.");
+
+            var restart = true;
+            while (restart)
+            {
+                var countRightAnswers = 0;
+                var questionNumber = DiagnosticTestResources.GetMixiForQuestion();
+                for (var i = 0 ; i < DiagnosticTestResources.Questions.Count ; i++)
+                {
+                    Console.WriteLine($"Вопрос номер: {i + 1}");
+                    var numberQuestion = questionNumber[i];
+                    Console.WriteLine(DiagnosticTestResources.Questions[numberQuestion]);
+                    var returnNumberUser = true;
+                    while (returnNumberUser)
+                    {
+                        try
+                        {
+                            if (DiagnosticTestResources.Answers[numberQuestion] == short.Parse(Console.ReadLine()))
+                                countRightAnswers++;
+                            returnNumberUser = false;
+                        }
+                        catch
+                        {
+                            Console.WriteLine($"{userName}, вы ввели букву, а нужно число, введите корректное число не длинее 6 знаков.");
+                            returnNumberUser = true;
+                        }
+                    }
+                }
+                Console.WriteLine($"{userName}, вы ответили верно на {countRightAnswers} вопросов.\nВаш результат {DiagnosticTestResources.Diagnoses[countRightAnswers]}.");
+                Console.WriteLine($"{userName} хотите пройти тест еще раз?");
+                restart = DiagnosticTestResources.GetRestartQuestion(Console.ReadLine(), userName);
+            }
+        }
+        public static string GetValidUserName()
+        {
+            while (true)
+            {
+                try
+                {
+                    var name = Console.ReadLine();
+                    return DiagnosticTestResources.UserInput(name);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Повторите попытку ввода еще раз.");
+                }
+            }
+        }
+    }
+
+}
